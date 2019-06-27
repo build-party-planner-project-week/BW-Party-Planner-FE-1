81b4f69bbe9f1b80ffe7021bee8b1ad0e268b650
Michael Harris <mhguitar1997@gmail.com>
2019-06-27 10:43:54 -0700

    Deltete comments

--- party-planner/src/utility/PrivateRoute.js
+++ party-planner/src/utility/PrivateRoute.js
@@ -2,12 +2,7 @@ import React from 'react'
 import {Route, Redirect} from 'react-router-dom'
 
 const PrivateRoute = ({component: Component, ...rest}) => {
-    //check if user is logged in and let them in component
-    // if(localStorage.getItem("token")){
-    //     return <Route render={props => <Component {...props} /> } />;
-    // } else {
-    //     return <Redirect to="/sign-in" />;
-    // }
+
     return <Route {...rest} render={props => {
         if(localStorage.getItem('token')){
             return <Component {...props} />
